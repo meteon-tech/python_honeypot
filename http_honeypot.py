152826fa306910d577a3f6c9fce76a5b1c29b012
meteon-tech <simekon22@sps-prosek.cz>
2025-12-28 20:08:31 +0100

    Enhance logging to CSV format with user agent
    Updated logging functionality to write logs in CSV format with timestamp, IP, port, method, path, status, and user agent.

--- http_honeypot.py
+++ http_honeypot.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 import socket
-import datetime
+from datetime import datetime, UTC
 import logging
+import csv
 
 HOST = '0.0.0.0'
 PORT = 8080
 LOG_FILE = 'honeypot_logs.csv'
+
+
+#Nastaveni zakladni konfigurace logovani
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
+def logCsv(timestamp, ip, port, method, path, status, userAgent):
+	row = [timestamp, ip, port, method, path, status, userAgent]
+	try:
+		with open(LOG_FILE, 'a') as file:
+			write = csv.writer(file)
+			write.writerow(row)
+	except Exception as e:
+		logging.warning('Failed to write to log file', e)
+
 
 def main():
 	print('Honeypot started...')
@@ -23,17 +38,26 @@ def main():
 			print(f'Session accepted by {addr[0]}:{addr[1]}\n')
 
 			data = client.recv(1024).decode()
-			#print(data)
 
-			parsedData = data.split()
-			print(parsedData)
-			print('\n')
-			path = None
-			for line in parsedData:
-				#print(line)
-				if line == '/admin':
-					path = line
-					#print(path)
+			#Rozdeli data, ziskam GET / HTTP1.1 jako jeden celek
+			parsedData = data.split('\r\n')
+			#Rozdelim data na jednotlive kusy GET, /, HTTP1.1
+			parts = parsedData[0].split()
+
+			#Zjistim jestli list ma prvky
+			method = parts[0] if len(parts) > 0 else 'UNKNOWN'
+			path = parts[1] if len(parts) > 1 else '/'
+
+			#print(path)
+			#print(parsedData)
+			#Hledani user-agenta v prijmutych a dekodovanych datech
+			userAgent = 'UNKNOWN'
+			for item in parsedData:
+				if item.lower().startswith('user-agent'):
+					#.split() rozdeli podle : ale jenom jednou (:, 1)
+					#.strip() ocisti data na zacatku a na konci od neviditelnych znaku
+					userAgent = item.split(':', 1)[1].strip()
+
 			if path == '/admin':
 				httpResponseAdmin = (
 					"HTTP/1.1 200 OK\r\n"
@@ -43,6 +67,7 @@ def main():
 					"<html><body><h1>Admin page</h1></body></html>"
 				)
 				client.send(httpResponseAdmin.encode())
+				status = 200
 			else:
 				httpResponse = (
 					"HTTP/1.1 404 Not Found\r\n"
@@ -52,6 +77,8 @@ def main():
 					"<html><body><h1>404 Not Found</h1></body></html>"
 				)
 				client.send(httpResponse.encode())
+				status = 404
+			logCsv(datetime.now(UTC), addr[0], addr[1], method, path, status, userAgent)
 
 	except KeyboardInterrupt:
 		print('Honeypot was stopped by user')
