edb6129fe2765e2f3fbe84269703e34f8736947c
meteon-tech <simekon22@sps-prosek.cz>
2026-01-20 12:27:40 +0100

    Implement CSV logging for FTP authentication attempts
    Added logging functionality to record authentication attempts in a CSV file.

--- ftp_honeypot.py
+++ ftp_honeypot.py
@@ -1,8 +1,14 @@
-#!/usr/bin/python3
 import os
+import csv
+import logging
+import datetime
 from pyftpdlib.authorizers import DummyAuthorizer, AuthenticationFailed
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+
+LOG_FILE = 'honeypot_ftp_logs.csv'
+PORT = 2121
+HOST = '0.0.0.0'
 
 class MyAuthorizer(DummyAuthorizer):
 	def validate_authentication(self, username, password, handler):
@@ -10,8 +16,16 @@ class MyAuthorizer(DummyAuthorizer):
 		port = handler.remote_port
 
 		print(ip, port, username, password)
-
+		logCsv(datetime.datetime.now(), ip, port, username, password)
 		raise AuthenticationFailed('Authentication failed')
+
+
+def logCsv(timestamp, ip, port, username, password):
+	row = [timestamp, ip, port, username, password]
+	with open(LOG_FILE, 'a') as file:
+		write = csv.writer(file)
+		write.writerow(row)
+
 
 authorizer = MyAuthorizer()
 
@@ -20,7 +34,7 @@ handler.authorizer = authorizer
 
 handler.banner = 'Hello'
 
-address = ('0.0.0.0', 2121)
+address = (HOST, PORT)
 server = FTPServer(address, handler)
 
 server.max_cons = 256
