2e271c4d22e7acc11e402b20924a58c3768ca6ce
meteon-tech <simekon22@sps-prosek.cz>
2026-01-26 18:54:17 +0100

    Refactor telnet honeypot for threading and logging
    Refactor telnet honeypot to use threading instead of ThreadPoolExecutor, implement connection limit, and improve logging.

--- telnet_honeypot.py
+++ telnet_honeypot.py
@@ -1,61 +1,88 @@
 #!/usr/bin/python3
 import socket
 import logging
-import csv
 import datetime
-from concurrent.futures import ThreadPoolExecutor
+import threading
 
 HOST = '0.0.0.0'
 PORT = 2323
 LOG_FILE = 'honeypot_telnet_logs.csv' 
 BANNER = b'Linux 3.10.14 armv7l\r\n\r\n# '
-
-def logCsv(timestamp, ip, port, command):
-	row = [timestamp, ip, port, command]
-	try:
-		with open(LOG_FILE, 'a') as file:
-			write = csv.writer(file)
-			write.writerow(row)
-	except Exception as e:
-		print(f'Failed to write to log file: {e}')
-
+MAX_CONNECTIONS = 2
+threadLimiter = threading.Semaphore(MAX_CONNECTIONS)
 
 def handleClient(client, addr):
-	print(addr[0], addr[1])
+	ip = addr[0]
+	port = addr[1]
+	logging.info(f"Connection made from: {ip}:{port}")
 	try:
 		client.settimeout(30)
 		client.send(BANNER)
 		while True:
-			data = client.recv(1024)
-			if not data:
+			try:
+				data = client.recv(1024)
+				if not data:
+					break
+				print(data)
+				if data.startswith(b'\xff'):
+					break
+
+				command = data.decode(errors='ignore').strip()
+
+				if not command:
+					client.send(b'# ')
+					continue
+				logging.info(f"{ip},{port},{command}")
+				if command.lower() == 'exit':
+					break
+				elif command.lower() == 'whoami':
+					client.send(b'root\r\n')
+				elif command.lower() == 'ls':
+					client.send(b'bin\tdev\tetc\thome\tlib\tproc\troot\ttmp\tvar\r\n')
+				client.send(b'# ')
+			except ConnectionResetError:
+				print('klient nasilne ukoncil komunikaci')
 				break
-			print(data)
-			command = data.decode().strip()
-			logCsv(datetime.datetime.now(), addr[0], addr[1], command)
-			if command.lower() == 'exit':
+			except socket.timeout:
+				print('connection timed out')
 				break
-			elif command.lower() == 'whoami':
-				client.send(b'root\r\n')
-			elif command.lower() == 'ls':
-				client.send(b'bin	dev	etc	home	lib	proc	root	tmp	var\r\n')
-			client.send(b'# ')
 	except Exception as e:
 		print(e)
 	finally:
+		print('finnaly')
 		client.close()
+		threadLimiter.release()
 
 
 def main():
+	logFormat = logging.Formatter('%(asctime)s,%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+	fileHandler = logging.FileHandler(LOG_FILE, mode='a', encoding='utf-8')
+	fileHandler.setFormatter(logFormat)
+
+	logger = logging.getLogger()
+	logger.setLevel(logging.INFO)
+	logger.addHandler(fileHandler)
+
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	server.bind((HOST, PORT))
-	server.listen(10)
+	server.listen(5)
+	server.settimeout(1)
 
 	try:
-		with ThreadPoolExecutor(max_workers=10) as executor:
-			while True:
+		while True:
+			try:
 				client, addr = server.accept()
-				executor.submit(handleClient, client, addr)
+				if threadLimiter.acquire(blocking=False):
+					thread = threading.Thread(target=handleClient, args=(client, addr))
+					thread.daemon = True
+					thread.start()
+				else:
+					print('connection refused')
+					client.close()
+			except socket.timeout:
+				continue
 	except KeyboardInterrupt:
 		print('Telnet was stopped by user')
 	finally:
