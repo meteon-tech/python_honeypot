c98463c1498a6f9afb764419593516ca4b9e5a58
meteon-tech <simekon22@sps-prosek.cz>
2026-01-31 18:32:35 +0100

    Enhance client handling and buffer management
    Refactor client handling in telnet honeypot to improve buffer management and command processing.

--- telnet_honeypot.py
+++ telnet_honeypot.py
@@ -9,43 +9,64 @@ PORT = 2323
 LOG_FILE = 'honeypot_telnet_logs.csv' 
 BANNER = b'Linux 3.10.14 armv7l\r\n\r\n# '
 MAX_CONNECTIONS = 2
+MAX_CONNECTION_SIZE = 4096
+
 threadLimiter = threading.Semaphore(MAX_CONNECTIONS)
 
 def handleClient(client, addr):
 	ip = addr[0]
 	port = addr[1]
 	logging.info(f"Connection made from: {ip}:{port}")
+	buffer = bytearray()
+	size = 0
 	try:
 		client.settimeout(30)
 		client.send(BANNER)
 		while True:
-			try:
-				data = client.recv(1024)
-				if not data:
-					break
-				print(data)
-				if data.startswith(b'\xff'):
-					break
+			data = client.recv(1024)
+			print(data.hex())
+			if not data:
+				break
+			print(data)
 
-				command = data.decode(errors='ignore').strip()
+			size += len(data)
+			if size > MAX_CONNECTION_SIZE:
+				break
+
+			buffer.extend(data)
+
+			if b'\xff\xf4' in buffer:
+				return
+
+			while b'\n' in buffer:
+
+				line, buffer = buffer.split(b'\n', 1)
+
+				line = line.replace(b'\r', b'')
+
+				if line.startswith(b'\xff'):
+					client.send(b'# ')
+					continue
+
+				command = line.decode(errors='replace').strip()
+				#if command.startswith('\xff'):
+				#	return
+
+				#command = data.decode(errors='ignore').strip()
 
 				if not command:
 					client.send(b'# ')
 					continue
 				logging.info(f"{ip},{port},{command}")
 				if command.lower() == 'exit':
-					break
+					return
 				elif command.lower() == 'whoami':
 					client.send(b'root\r\n')
 				elif command.lower() == 'ls':
 					client.send(b'bin\tdev\tetc\thome\tlib\tproc\troot\ttmp\tvar\r\n')
 				client.send(b'# ')
-			except ConnectionResetError:
-				print('klient nasilne ukoncil komunikaci')
-				break
-			except socket.timeout:
-				print('connection timed out')
-				break
+	except socket.timeout:
+		pass
 	except Exception as e:
 		print(e)
 	finally:
